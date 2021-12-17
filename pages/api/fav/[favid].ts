06041d0c5671f9c09e1d48096b8d18d58fc1b39a
RK <kicvong@gmail.com>
2021-12-17 08:18:48 +0700

    api route upsert & get Fav

--- pages/api/fav/[favid].ts
+++ pages/api/fav/[favid].ts
@@ -12,14 +12,38 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>) {
 
-    const { proid } = req.query;
+    const { favid } = req.query;
 
+
+    // create or update list fav by favid
     if (req.method === "POST") {
         try {
-            const product = req.body;
+            const favItems = req.body;
+            const userid = { "user": favid };
+            const upsert = { upsert: true };
+            const update = { $set: { "favorite": favItems } };
             let { db } = await connectToDatabase();
-            await db.collection("db").insertOne({ "user": "merry", "favorite": ["a 1 ea", "b 2 ea"] });
-            return res.status(200).json({ status: { code: 200, message: 'OK' }, data: "" });
+            let result = await db.collection("db").updateOne(userid, update, upsert);
+            return res.status(200).json({ status: { code: 200, message: 'OK' }, data: result });
+        } catch (error) {
+            return res.json({ message: error, success: false })
+        }
+    }
+
+
+    // get After login and create fav list or what ever 
+    if (req.method === "GET") {
+        try {
+            const query = { "user": favid };
+            let { db } = await connectToDatabase();
+            const result = await db.collection("db").findOne(query);
+
+            if (result == null) {
+                return res.status(404).json({ message: "No item found" })
+            }
+
+            return res.status(200).send(result);
+
         } catch (error) {
             return res.json({ message: error, success: false })
         }
